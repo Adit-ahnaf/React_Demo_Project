c63f2e213a7fd02f73386f845c8eadf85d314503
Adit-ahnaf <aditahnaf73@gmail.com>
2021-07-10 12:39:12 +0600

    signup_updated

--- src/Practice/Login.js
+++ src/Practice/Login.js
@@ -26,8 +26,7 @@ const Login = (props) => {
                       
                   </div>
                   <div class="flex items-center justify-between">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-  onClick={()=>{khuljasimsim()}} type="button">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
         Sign In
       </button>
       <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/sighnup">
